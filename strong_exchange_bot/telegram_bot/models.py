2bd75ef7d2cd5b5580143012cb573a012e79eb3b
Egor Sergeevich Poderiagin <egor@crazyrussian.pro>
2021-12-05 23:26:27 +0700

    bump requirements

--- strong_exchange_bot/telegram_bot/models.py
+++ strong_exchange_bot/telegram_bot/models.py
@@ -1,10 +1,8 @@
 import uuid
 from django.db import models
-from django.contrib.postgres.fields import JSONField
 
 
 class Update(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     update_id = models.PositiveIntegerField()
-    message = JSONField(null=True)
-
+    message = models.JSONField(null=True)
